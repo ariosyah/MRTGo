3ee8866264d5a6a1bf791cbb6893614cfd6041a8
ariosyahputra <ariosyh@gmail.com>
2023-07-18 10:02:44 +0700

    Initial Commit

---
+++ MRTGo/MRTGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRTGoApp.swift
+//  MRTGo
+//
+//  Created by Ario Syahputra on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MRTGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
