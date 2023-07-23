92bf79569ed5bab864e4dd0a43597bd32b069072
ariosyahputra <ariosyh@gmail.com>
2023-07-24 00:41:57 +0700

    added location detail

--- MRTGo/View/Component/Destination/DestinationListHelper.swift
+++ MRTGo/View/Component/Destination/DestinationListHelper.swift
@@ -31,11 +31,13 @@ func matchedDestination(destinationPlace: [Destination], destination: String, is
 //]
 
 let destinationPlace = [
-    // Pintu A
-//    Destination(name: "Plaza Indonesia", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)"),
-//    Destination(name: "Hotel Indonesia Kempinski", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)"),
-//    Destination(name: "Grand Indonesia", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)"),
-//    Destination(name: "Monumen Selamat Datang", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)"),
+    // Pintu A, 5 First Provided Location
+    Destination(name: "Plaza Indonesia", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)", location: (-6.193570566404313, 106.82194737519785)),
+    Destination(name: "Hotel Indonesia Kempinski", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)", location: (-6.195314240905304, 106.82232500903086)),
+    Destination(name: "Grand Indonesia", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)", location: (-6.194843875582314, 106.82095516686233)),
+    Destination(name: "Bundaran HI", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)", location: (-6.195081142046306, 106.823019743916)),
+    Destination(name: "Hotel Pullman Jakarta", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)", location: (-6.1931170, 106.8237489)),
+//    Destination(name: "Monumen Selamat Datang", exitGate: "Stasiun Bundaran HI (Exit 1 - Pintu A)")
 //    // Pintu B
 //    Destination(name: "Gedung Indonesia One", exitGate: "Stasiun Bundaran HI (Exit 2 - Pintu B)"),
 //    Destination(name: "Kedubes Jepang", exitGate: "Stasiun Bundaran HI (Exit 2 - Pintu B)"),
@@ -45,10 +47,8 @@ let destinationPlace = [
 //    Destination(name: "Halte Transjakarta", exitGate: "Stasiun Bundaran HI (Exit 3 - Pintu C)"),
 //    // Pintu D
 //    Destination(name: "Plaza Bank Index", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
-    Destination(name: "Hotel Pullman Jakarta", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)",location: (-6.1931170,106.8237489)),
 //    Destination(name: "Wisma Nusantara", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
 //    Destination(name: "Kantor PT MRT Jakarta", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
-//    Destination(name: "Bundaran HI", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
 //    Destination(name: "Jalan Sultan Syahrir", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
 //    Destination(name: "Polsubsektor Thamrin", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
 //    Destination(name: "Deutsche Bank", exitGate: "Stasiun Bundaran HI (Exit 4 - Pintu D)"),
@@ -63,5 +63,4 @@ let destinationPlace = [
 //    Destination(name: "Jalan Sunda", exitGate: "Stasiun Bundaran HI (Exit 6 - Pintu F)"),
 //    Destination(name: "Sarinah", exitGate: "Stasiun Bundaran HI (Exit 6 - Pintu F)"),
 //    Destination(name: "Jalan KH Wahid Hasyim", exitGate: "Stasiun Bundaran HI (Exit 6 - Pintu F)")
-
 ]
